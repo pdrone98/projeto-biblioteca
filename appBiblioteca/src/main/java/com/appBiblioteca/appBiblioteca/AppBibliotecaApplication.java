5f9f680cf0b5b12385ebc3245d688842b85a4b84
Gabriel Fabril Pedrone <gabrielpedrone@gmail.com>
2024-08-19 22:29:05 -0300

    Alterações no pom.xml e no arquivo da aplicação

--- appBiblioteca/src/main/java/com/appBiblioteca/appBiblioteca/AppBibliotecaApplication.java
+++ appBiblioteca/src/main/java/com/appBiblioteca/appBiblioteca/AppBibliotecaApplication.java
@@ -2,10 +2,14 @@ package com.appBiblioteca.appBiblioteca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.sql.SQLOutput;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.appBiblioteca.appBiblioteca"} )
 public class AppBibliotecaApplication {
 
 	public static void main(String[] args) {
